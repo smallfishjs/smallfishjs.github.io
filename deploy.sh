e8ab1448e47d24cf2690bf3132ca271aca4d9f13
nikogu <644506165@qq.com>
2019-06-04 11:01:13 +0800

    deploy.sh to 777

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push --set-upstream https://github.com/smallfishjs/smallfishjs.github.io.git master
+git push --set-upstream https://github.com/smallfishjs/smallfishjs.github.io.git master --force
 
 cd -
